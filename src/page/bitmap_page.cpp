68a204090eef8a743bdcaf300e3a101af457e80c
LanceLeonhart <134621933+LanceLeonhart@users.noreply.github.com>
2023-05-27 16:15:15 +0800

    Update bitmap_page.cpp
    finish 1.2 part and pass the test, upload the code

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -7,23 +7,59 @@
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  return false;
+  page_offset = 0;
+  uint32_t byte_index = page_offset / 8;  // 对应所在字节
+  uint8_t  bit_index = page_offset % 8;   // 字节里对应的位
+
+  while ( (bytes[byte_index] & (1 << bit_index)) != 0 )
+  {
+    page_offset++;
+    if (page_offset >= 8 * MAX_CHARS)
+    {
+      return false;
+    }
+    byte_index = page_offset / 8;
+    bit_index = page_offset % 8;
+  }
+
+  bytes[byte_index] |= (1 << bit_index);
+  return true;
 }
+
 
 /**
  * TODO: Student Implement
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  return false;
+  uint32_t byte_index = page_offset / 8;  // 对应所在字节
+  uint8_t bit_index = page_offset % 8;   // 字节里对应的位
+
+  if ((bytes[byte_index] & (1 << bit_index)) != 0) {
+    bytes[byte_index] = bytes[byte_index] & (~(1 << bit_index));
+    next_free_page_ = page_offset;
+    page_allocated_--;
+    return true;
+  } else {
+    return false; // 没有分配，不用回收
+  }
 }
+
 
 /**
  * TODO: Student Implement
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  return false;
+  uint32_t byte_index = page_offset / 8;  // 对应所在字节
+  uint8_t  bit_index = page_offset % 8;   // 字节里对应的位
+
+  if ( (bytes[byte_index] & (1 << bit_index)) == 0)
+  {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 template <size_t PageSize>
